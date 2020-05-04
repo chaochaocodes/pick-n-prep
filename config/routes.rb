196e7b76b2af4b03770748b40c9d48af7b01b740
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-05-03 23:12:15 -0400

    add: ingredients, directions

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
+  resources :recipes
+  resources :ingredients
+  resources :directions
   resources :cuisines, only: [:index, :show]
-  resources :dishes
   resources :meals, only: [:index, :show]
-  resources :users
-    
-  root "meals#index"
-  # root "auth#landing"
-  get '/landing', to: "auth#landing", as: "landing"
+  
+  # root "meals#index"
+  root "auth#landing"
+  # get '/landing', to: "auth#landing", as: "landing"
   get '/signup', to: "users#new", as: "signup"
   get '/login', to: "auth#login", as: "login" #auth#new
   post '/login', to: "auth#authenticate"  #auth#create
   get '/logout', to: "auth#logout" 
+  resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
