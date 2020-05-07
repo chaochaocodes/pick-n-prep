8d1c5b409abe6b377cce7c1c007243a866d459f9
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-05-07 13:53:37 -0400

    yarn add cocoon w/ Rails 6

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module PickNPrep
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 6.0
-    config.autoloader = :classic
+    config.load_defaults 6.0
+    # config.autoloader = :classic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
