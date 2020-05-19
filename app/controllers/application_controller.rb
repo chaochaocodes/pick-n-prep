34d598f6736ca02c3d2168271639af17c41556ef
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-05-19 16:21:46 -0400

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-    helper_method :logged_in?, :check_user
+    helper_method :logged_in?, :check_user, :current_user
 
-    # def current_user
-    #     @current_user ||= User.find(session[:user_id]) if session[:user_id] 
-    # end
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+    end
     
     def check_user
         if !logged_in?
